46bfca91a6af387793fba4aa42bda5fbd14bf065
REDOWAN038 <redowanibrahim2724@gmail.com>
2025-07-31 00:58:00 +0600

    user authentication

--- stayswift/components/Navbar.jsx
+++ stayswift/components/Navbar.jsx
@@ -1,41 +1,58 @@
-
 import Link from "next/link"
 import Image from "next/image"
+import { auth } from "@/auth"
+import Logout from "./auth/Logout"
 
-const Navbar = () => {
-  return (
-    <nav>
-    <Link href="/">
-      <Image 
-        src="/stayswift.svg" 
-        alt="Stay Swift Logo" 
-        width={200}
-        height={200} />
-    </Link>
+const Navbar = async ({ sideMenu }) => {
+    const session = await auth()
+    return (
+        <nav>
+            <Link href='/'>
+                <Image
+                    src='/stayswift.svg'
+                    alt='Stay Swift Logo'
+                    width={200}
+                    height={200}
+                />
+            </Link>
+            {sideMenu && (
+                <ul>
+                    <li>
+                        <Link href='#'>Recommended Places</Link>
+                    </li>
 
-    <ul>
-      <li>
-        <Link href="#">Recommended Places</Link>
-      </li>
+                    <li>
+                        <Link href='#'>About Us</Link>
+                    </li>
 
-      <li>
-        <Link href="#">About Us</Link>
-      </li>
+                    <li>
+                        <Link href='#'>Contact us</Link>
+                    </li>
 
-      <li>
-        <Link href="#">Contact us</Link>
-      </li>
+                    <li>
+                        <Link href='/bookings'>Bookings</Link>
+                    </li>
 
-      <li>
-        <Link href="/bookings">Bookings</Link>
-      </li>
-
-      <li>
-        <Link href="/login" class="login">Login</Link>
-      </li>
-    </ul>
-  </nav>
-  )
+                    <li>
+                        {session?.user ? (
+                            <idiv>
+                                <span className='mx-1'>
+                                    {" "}
+                                    {session?.user?.name}{" "}
+                                </span>
+                                <span> | </span>
+                                <Logout />
+                            </idiv>
+                        ) : (
+                            <Link href='/login' className='login'>
+                                Login
+                            </Link>
+                        )}
+                    </li>
+                </ul>
+            )}
+        </nav>
+    )
 }
 
 export default Navbar
